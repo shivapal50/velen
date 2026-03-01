2422d4e15f3916dd7dbfb3355364620ff8368557
shivapal50 <shivapal008@gmail.com>
2026-03-01 11:40:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,21 @@ console.log('PORT:', PORT);
 
 // Middleware with proper CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5000'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://velen-o7nr.onrender.com'
+    ];
 
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Allowed users list
